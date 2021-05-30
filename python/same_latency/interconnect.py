ce0251a71bc374a4f7bb51218ebef82d0c50ecea
knyuchen <knyuchen@umich.edu>
2021-05-30 19:15:34 -0400

    add single row function

--- python/same_latency/interconnect.py
+++ python/same_latency/interconnect.py
@@ -4,7 +4,7 @@ global io_file
 io_file = open("./same_latency.sv", "w+")
 
 
-def single_21cell (up_text, up_num, down_text, down_num, indi, up_pipe, down_pipe):
+def single_21_cell (up_text, up_num, down_text, down_num, indi, up_pipe, down_pipe):
    io_file.write('   sl21_cell #(\n')
    io_file.write('      .INDI('+str(indi)+'),\n')   
    io_file.write('      .UP_PIPE('+str(up_pipe)+'),\n')   
@@ -20,7 +20,7 @@ def single_21cell (up_text, up_num, down_text, down_num, indi, up_pipe, down_pip
    io_file.write('      .*\n')
    io_file.write('   );\n\n')
 
-def single_11cell (up_text, up_num, down_text, down_num, up_pipe, down_pipe):
+def single_11_cell (up_text, up_num, down_text, down_num, up_pipe, down_pipe):
    io_file.write('   sl11_cell #(\n')
    io_file.write('      .UP_PIPE('+str(up_pipe)+'),\n')   
    io_file.write('      .DOWN_PIPE('+str(down_pipe)+'),\n')   
@@ -33,5 +33,13 @@ def single_11cell (up_text, up_num, down_text, down_num, up_pipe, down_pipe):
    io_file.write('      .*\n')
    io_file.write('   );\n\n')
 
-single_21cell ('sys', 2, 'cis', 4, 5, 1, 2) 
-single_11cell ('sys', 3, 'cis', 5,    1, 2) 
+def single_row (num, up_text, up_num, down_text, down_num, indi, up_pipe, down_pipe):
+   num_of_21 = int(num/2)
+   num_of_11 = num%2
+   for i in range (num_of_21):
+      single_21_cell (up_text, up_num+i, down_text, down_num+2*i, indi, up_pipe, down_pipe)
+   if num_of_11 != 0:
+      single_11_cell (up_text, up_num+num_of_21, down_text, down_num+num-1, up_pipe, down_pipe)
+   io_file.write('\n\n')
+
+single_row (10, 'sys', 2, 'cis', 4, 5, 1, 2) 
--- python/same_latency/same_latency.sv
+++ python/same_latency/same_latency.sv
@@ -13,15 +13,65 @@
       .*
    );
 
-   sl11_cell #(
+   sl21_cell #(
+      .INDI(5),
       .UP_PIPE(1),
       .DOWN_PIPE(2),
    )
-   sl11_sys_3_cis_5(
+   sl21_sys_3_cis_6(
       .req_up(req_sys_3),
       .res_up(res_sys_3),
-      .req_down(req_cis_5),
-      .res_down(res_cis_5),
+      .req_down0(req_cis_6),
+      .res_down0(res_cis_6),
+      .req_down1(req_cis_7),
+      .res_down1(res_cis_7),
       .*
    );
+
+   sl21_cell #(
+      .INDI(5),
+      .UP_PIPE(1),
+      .DOWN_PIPE(2),
+   )
+   sl21_sys_4_cis_8(
+      .req_up(req_sys_4),
+      .res_up(res_sys_4),
+      .req_down0(req_cis_8),
+      .res_down0(res_cis_8),
+      .req_down1(req_cis_9),
+      .res_down1(res_cis_9),
+      .*
+   );
+
+   sl21_cell #(
+      .INDI(5),
+      .UP_PIPE(1),
+      .DOWN_PIPE(2),
+   )
+   sl21_sys_5_cis_10(
+      .req_up(req_sys_5),
+      .res_up(res_sys_5),
+      .req_down0(req_cis_10),
+      .res_down0(res_cis_10),
+      .req_down1(req_cis_11),
+      .res_down1(res_cis_11),
+      .*
+   );
+
+   sl21_cell #(
+      .INDI(5),
+      .UP_PIPE(1),
+      .DOWN_PIPE(2),
+   )
+   sl21_sys_6_cis_12(
+      .req_up(req_sys_6),
+      .res_up(res_sys_6),
+      .req_down0(req_cis_12),
+      .res_down0(res_cis_12),
+      .req_down1(req_cis_13),
+      .res_down1(res_cis_13),
+      .*
+   );
+
+
 
