843e1eea18fd6693c0fad64acd5157a03019f9ab
knyuchen <knyuchen@umich.edu>
2021-06-05 10:08:22 -0400

    finish generating whole module

--- python/same_latency/interconnect.py
+++ python/same_latency/interconnect.py
@@ -58,6 +58,58 @@ def single_pyramid (num, text, indi, up_pipe, down_pipe):
       current_indi = current_indi + 1
    return current_down_num
 
+def find_num (num):
+   final = num #top & bottom
+   while (num > 1):
+      num = math.ceil(num/2)
+      final = final + num
+   return final
+
+def intermediate_logic (num, logic, name):
+   for i in range (num):
+      io_file.write('   ' + logic + '   ' + name + '_' + str (i) + ';\n')
+   io_file.write('\n\n')  
+
+def module_header (num, up_name, down_name):
+    io_file.write('module sl_interconnect_' + str(num) +' (')
+    io_file.write('\n')
+    io_file.write('   input     SL_REQ   ' + up_name + '_req,\n')
+    io_file.write('   output    SL_RES   ' + up_name + '_res,\n')
+    for i in range (num) :
+        io_file.write('   output    SL_REQ   ' + down_name + '_req_' + str(i) + ',\n')
+        io_file.write('   input     SL_RES   ' + down_name + '_res_' + str(i) + ',\n')
+    io_file.write('   input   clk,')
+    io_file.write('\n')
+    io_file.write('   input   rst_n')
+    io_file.write('\n')
+    io_file.write(');')
+
+    io_file.write('\n')
+    io_file.write('\n')
+def module_finish():
+    io_file.write('endmodule\n')
+def inter_name (total, num, up_name, down_name, inter_name):
+    io_file.write('assign  ' + up_name + '_res = ' + inter_name + '_res_' + str (total-1) + ';\n')
+    io_file.write('assign  ' + inter_name + '_req_' + str(total-1) + ' = ' + up_name + '_req;\n')
+    for i in range (num):
+        io_file.write('assign  ' + down_name + '_req_' + str(i) +' = ' + inter_name + '_req_' + str(i) + ';\n')
+        io_file.write('assign  ' + inter_name + '_res_' + str(i) +' = ' + down_name + '_res_' + str(i) + ';\n')
+    io_file.write('\n')
+
+def interconnect_module (num, up_name, down_name, indi, up, down):
+    module_header (num, up_name, down_name)
+    total = find_num(num)
+    intermediate_logic (num, 'SL_REQ', 'inter_req')
+    intermediate_logic (num, 'SL_RES', 'inter_res')
+    inter_name(total, num, up_name, down_name, 'inter')        
+    single_pyramid(num, 'inter', indi, up, down)
+    module_finish()
+
 up = [1, 1, 2, 2]
 down = [2, 2, 1, 1]
-single_pyramid (10, 'sys', 5, up, down)
+#print(find_num (10))
+#intermediate_logic (10, 'pp', 'qq')
+#single_pyramid (10, 'sys', 5, up, down)
+
+interconnect_module (4, 'host', 'slave', 12, up, down)
+
--- python/same_latency/same_latency.sv
+++ python/same_latency/same_latency.sv
@@ -1,167 +1,88 @@
+module sl_interconnect_4 (
+   input     SL_REQ   host_req,
+   output    SL_RES   host_res,
+   output    SL_REQ   slave_req_0,
+   input     SL_RES   slave_res_0,
+   output    SL_REQ   slave_req_1,
+   input     SL_RES   slave_res_1,
+   output    SL_REQ   slave_req_2,
+   input     SL_RES   slave_res_2,
+   output    SL_REQ   slave_req_3,
+   input     SL_RES   slave_res_3,
+   input   clk,
+   input   rst_n
+);
+
+   SL_REQ   inter_req_0;
+   SL_REQ   inter_req_1;
+   SL_REQ   inter_req_2;
+   SL_REQ   inter_req_3;
+
+
+   SL_RES   inter_res_0;
+   SL_RES   inter_res_1;
+   SL_RES   inter_res_2;
+   SL_RES   inter_res_3;
+
+
+assign  host_res = inter_res_6;
+assign  inter_req_6 = host_req;
+assign  slave_req_0 = inter_req_0;
+assign  inter_res_0 = slave_res_0;
+assign  slave_req_1 = inter_req_1;
+assign  inter_res_1 = slave_res_1;
+assign  slave_req_2 = inter_req_2;
+assign  inter_res_2 = slave_res_2;
+assign  slave_req_3 = inter_req_3;
+assign  inter_res_3 = slave_res_3;
+
    sl21_cell #(
-      .INDI(5),
+      .INDI(12),
       .UP_PIPE(1),
       .DOWN_PIPE(2),
    )
-   sl21_sys_10_sys_0(
-      .req_up(req_sys_10),
-      .res_up(res_sys_10),
-      .req_down0(req_sys_0),
-      .res_down0(res_sys_0),
-      .req_down1(req_sys_1),
-      .res_down1(res_sys_1),
+   sl21_inter_4_inter_0(
+      .req_up(req_inter_4),
+      .res_up(res_inter_4),
+      .req_down0(req_inter_0),
+      .res_down0(res_inter_0),
+      .req_down1(req_inter_1),
+      .res_down1(res_inter_1),
       .*
    );
 
    sl21_cell #(
-      .INDI(5),
+      .INDI(12),
       .UP_PIPE(1),
       .DOWN_PIPE(2),
    )
-   sl21_sys_11_sys_2(
-      .req_up(req_sys_11),
-      .res_up(res_sys_11),
-      .req_down0(req_sys_2),
-      .res_down0(res_sys_2),
-      .req_down1(req_sys_3),
-      .res_down1(res_sys_3),
+   sl21_inter_5_inter_2(
+      .req_up(req_inter_5),
+      .res_up(res_inter_5),
+      .req_down0(req_inter_2),
+      .res_down0(res_inter_2),
+      .req_down1(req_inter_3),
+      .res_down1(res_inter_3),
       .*
    );
 
+
+
    sl21_cell #(
-      .INDI(5),
+      .INDI(13),
       .UP_PIPE(1),
       .DOWN_PIPE(2),
    )
-   sl21_sys_12_sys_4(
-      .req_up(req_sys_12),
-      .res_up(res_sys_12),
-      .req_down0(req_sys_4),
-      .res_down0(res_sys_4),
-      .req_down1(req_sys_5),
-      .res_down1(res_sys_5),
-      .*
-   );
-
-   sl21_cell #(
-      .INDI(5),
-      .UP_PIPE(1),
-      .DOWN_PIPE(2),
-   )
-   sl21_sys_13_sys_6(
-      .req_up(req_sys_13),
-      .res_up(res_sys_13),
-      .req_down0(req_sys_6),
-      .res_down0(res_sys_6),
-      .req_down1(req_sys_7),
-      .res_down1(res_sys_7),
-      .*
-   );
-
-   sl21_cell #(
-      .INDI(5),
-      .UP_PIPE(1),
-      .DOWN_PIPE(2),
-   )
-   sl21_sys_14_sys_8(
-      .req_up(req_sys_14),
-      .res_up(res_sys_14),
-      .req_down0(req_sys_8),
-      .res_down0(res_sys_8),
-      .req_down1(req_sys_9),
-      .res_down1(res_sys_9),
+   sl21_inter_6_inter_4(
+      .req_up(req_inter_6),
+      .res_up(res_inter_6),
+      .req_down0(req_inter_4),
+      .res_down0(res_inter_4),
+      .req_down1(req_inter_5),
+      .res_down1(res_inter_5),
       .*
    );
 
 
 
-   sl21_cell #(
-      .INDI(6),
-      .UP_PIPE(1),
-      .DOWN_PIPE(2),
-   )
-   sl21_sys_15_sys_10(
-      .req_up(req_sys_15),
-      .res_up(res_sys_15),
-      .req_down0(req_sys_10),
-      .res_down0(res_sys_10),
-      .req_down1(req_sys_11),
-      .res_down1(res_sys_11),
-      .*
-   );
-
-   sl21_cell #(
-      .INDI(6),
-      .UP_PIPE(1),
-      .DOWN_PIPE(2),
-   )
-   sl21_sys_16_sys_12(
-      .req_up(req_sys_16),
-      .res_up(res_sys_16),
-      .req_down0(req_sys_12),
-      .res_down0(res_sys_12),
-      .req_down1(req_sys_13),
-      .res_down1(res_sys_13),
-      .*
-   );
-
-   sl11_cell #(
-      .UP_PIPE(1),
-      .DOWN_PIPE(2),
-   )
-   sl11_sys_17_sys_14(
-      .req_up(req_sys_17),
-      .res_up(res_sys_17),
-      .req_down(req_sys_14),
-      .res_down(res_sys_14),
-      .*
-   );
-
-
-
-   sl21_cell #(
-      .INDI(7),
-      .UP_PIPE(2),
-      .DOWN_PIPE(1),
-   )
-   sl21_sys_18_sys_15(
-      .req_up(req_sys_18),
-      .res_up(res_sys_18),
-      .req_down0(req_sys_15),
-      .res_down0(res_sys_15),
-      .req_down1(req_sys_16),
-      .res_down1(res_sys_16),
-      .*
-   );
-
-   sl11_cell #(
-      .UP_PIPE(2),
-      .DOWN_PIPE(1),
-   )
-   sl11_sys_19_sys_17(
-      .req_up(req_sys_19),
-      .res_up(res_sys_19),
-      .req_down(req_sys_17),
-      .res_down(res_sys_17),
-      .*
-   );
-
-
-
-   sl21_cell #(
-      .INDI(8),
-      .UP_PIPE(2),
-      .DOWN_PIPE(1),
-   )
-   sl21_sys_20_sys_18(
-      .req_up(req_sys_20),
-      .res_up(res_sys_20),
-      .req_down0(req_sys_18),
-      .res_down0(res_sys_18),
-      .req_down1(req_sys_19),
-      .res_down1(res_sys_19),
-      .*
-   );
-
-
-
+endmodule
